b28e5d23a61bb622e858552f42d746156b00b28b
sztuczkap <p.sztuczka@wp.pl>
2021-01-04 22:19:07 +0100

    Stream API

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,32 @@
 package pl.sztuczkap;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // 1. Strumień z kolekcji
+        List<String> names = List.of("Ania", "Ola", "Tomek", "Adam");
+        Stream<String> stream = names.stream();
+
+        // 2. Strumień z Tablicy
+        String[] languages = new String[]{"Java", "Python", "C++"};
+        Stream<String> languagesStream = Stream.of(languages);
+        Stream<String> languagesStream2 = Arrays.stream(languages, 1, 2);
+
+        // 3. Strumień zero lub wiele elementów
+        Stream<String> emptyStream = Stream.empty();
+        Stream<String> elementsStream = Stream.of("One", "Two", "Three", "Four");
+
+        // 4. Nieskończone strumienie
+        Stream<String> infinityStream = Stream.generate(() -> "repeat");
+        Stream<Double> infinityRandomStream = Stream.generate(Math::random); // strumień nieskończonych liczb losowych
+        Stream<BigInteger> infinitySequence = Stream.iterate(BigInteger.ZERO, n -> n.add(BigInteger.ONE));  // nieskonczony ciąg 1,2,3...
+
+
     }
 }
